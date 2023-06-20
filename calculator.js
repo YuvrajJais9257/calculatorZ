315b25b42ba6e9146113f31e5c0e5f8596715883
Yuvraj Jaiswal <83688521+YuvrajJais9257@users.noreply.github.com>
2023-06-20 11:31:11 +0530

    Update calculator.js

--- calculator.js
+++ calculator.js
@@ -1,16 +1,16 @@
-// Function to display the clicked value on the screen
+
 function display(value) {
     var screen = document.getElementById("solution");
     screen.value += value;
 }
 
-// Function to clear the screen
+
 function clearCharacter() {
     var screen = document.getElementById("solution");
     screen.value = "";
 }
 
-// Function to perform the calculation and display the result
+
 function calculate() {
     var screen = document.getElementById("solution");
     var expression = screen.value;
